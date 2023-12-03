70a601c00428ec22009e272e75e01498373aa915
loki-099 <loidatolentino358@gmail.com>
2023-12-03 23:43:38 +0800

    edit Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
       <h1 className='logo-text font-bold text-white'>CALC<span className='text-acc'>HUB</span></h1>
       <nav>
         <ul className='hidden items-center gap-6 text-white xl:flex'>
-          <li className='heading-3 font-bold uppercase hover:text-acc hover:scale-125 transition-all ease-in-out'><a href="https://youtu.be/YNstP0ESndU?feature=shared">Limits</a></li>
-          <li className='heading-3 font-bold uppercase hover:text-acc hover:scale-125 transition-all ease-in-out'><a href="">Derivatives</a></li>
+          <li className='heading-3 font-bold uppercase hover:text-acc hover:scale-125 transition-all ease-in-out'><a href="https://youtu.be/YNstP0ESndU?feature=shared" target='_blank'>Limits</a></li>
+          <li className='heading-3 font-bold uppercase hover:text-acc hover:scale-125 transition-all ease-in-out'><a href="https://youtu.be/5yfh5cf4-0w?si=mZmnmMM6v6vjzHer" target='_blank'>Differentiation</a></li>
         </ul>
       </nav>
     </div>
