396b19f06c8d5c85227c49ec449ae88934847a91
loki-099 <loidatolentino358@gmail.com>
2023-12-05 19:02:19 +0800

    edit line-height

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className='relative w-full py-4 px-4 flex items-center justify-between z-10'>
-      <Link to="/"><h1 className='logo-text font-bold text-white'>CALC<span className='text-acc'>HUB</span></h1></Link>
+    <div className='relative w-full py-6 px-4 flex items-center justify-between z-10'>
+      <Link to="/" className='logo-text font-bold text-white'>CALC<span className='text-acc'>HUB</span></Link>
       <nav>
         <ul className='hidden items-center gap-6 text-white xl:flex'>
-          <Link to="/videos"><li className='heading-3 font-bold uppercase hover:text-acc hover:scale-125 transition-all ease-in-out'><a href="./videos">Limits</a></li></Link>
-          <Link to="/videos"><li className='heading-3 font-bold uppercase hover:text-acc hover:scale-125 transition-all ease-in-out'><a href="./videos">Differentiation</a></li></Link>
+          <li className='heading-3 font-bold uppercase hover:text-acc hover:scale-125 transition-all ease-in-out'><Link to="videos">Limits</Link></li>
+          <li className='heading-3 font-bold uppercase hover:text-acc hover:scale-125 transition-all ease-in-out'><Link to="videos">Differentiation</Link></li>
         </ul>
       </nav>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='absolute top-10 w-full h-full px-4 flex flex-col xl:flex-row items-center justify-evenly xl:gap-0 xl:justify-between'>
       <div className='xl:w-[48%] text-center xl:text-left'>
         <h1 className='heading-1 uppercase font-bold text-white leading-[1] mb-3'>Struggling with <span className='text-acc'>Calculus</span>?</h1>
-        <p className='parag text-white leading-[1.25] mb-5 hidden xl:block'>Follow the roadmaps for what you should work on more to enhance your mathematics skills, and watch videos that will help you along the way solving calculus.</p>
+        <p className='parag text-white leading-[1.25] mb-7 hidden xl:block'>Follow the roadmaps for what you should work on more to enhance your mathematics skills, and watch videos that will help you along the way solving calculus.</p>
         <a href="https://www.youtube.com/@TheOrganicChemistryTutor" className='button heading-3 text-white font-bold uppercase hover:opacity-80' target='_blank'>Click Here</a>
       </div>
       <div className='xl:w-[48%] sm:max-w-[450]'>
