e40d6142f5e455c36f4b5a7bf422945aae743aa5
mischiefluis <loidatolentino358@gmail.com>
2023-12-10 00:16:23 +0800

    git sucks

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -7,7 +7,7 @@ const Topics = () => {
     <div className='relative w-full py-2 px-4 z-10'>
       <nav className='w-full border-b-acc border-b-2'>
         <ul className='flex items-center gap-4'>
-          <li><NavLink to="limits" className='text-white heading-3 uppercase font-bold'>Limitss</NavLink></li>
+          <li><NavLink to="limits" className='text-white heading-3 uppercase font-bold'>Limits</NavLink></li>
           <li><NavLink to="differentiation" className='text-white heading-3 uppercase font-bold'>Differentiations</NavLink></li>
         </ul>
       </nav>
