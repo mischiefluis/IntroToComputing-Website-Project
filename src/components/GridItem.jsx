783b432ec92dadc517ef524bcd5947830122c585
mischiefluis <loidatolentino358@gmail.com>
2023-12-17 20:59:47 +0800

    added videos and pulsing loading

--- src/components/GridItem.jsx
+++ src/components/GridItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GridItem = ( {link} ) => {
   return (
     <div className='border-2 border-acc h-[190px] max-w-[345px] relative'>
-        <p className='parag text-acc absolute -z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>Video Loading...</p>
+        <p className='parag text-acc absolute -z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] animate-pulse'>Video Loading...</p>
         <iframe className='w-full h-full' src={link} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   )
